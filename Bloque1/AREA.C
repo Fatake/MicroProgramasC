ad13c6e59eb909a1d2036bbd5b158d93a1ebec12
Fatake <pace_lozano@hotmail.com>
2021-02-06 12:40:58 -0600

    Cambiaando Identacion

--- Bloque1/AREA.C
+++ Bloque1/AREA.C
@@ -4,21 +4,28 @@ int bas();
 int alt();
 int area(int alt,int bas);
 void tot(int area);
+
+/**
+ * Main
+ */
 int main(){
 	bien();
 	tot(area(alt(),bas()));
 	printf("\n");
-return 0;
+	return 0;
 }
+
 void bien(){
 	printf("Programa que calcula el area de un triangulo \n");
 }
+
 int bas(){
 	int base;
 	printf("\n Proporcione una Base\n ->");
 	scanf("%d", &base);
 	return base;
 }
+
 int alt(){
 	int al;
 	printf("\n Proporcione una altura \n ->");
